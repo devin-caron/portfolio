01025c98598fdca93de80a7950d4fa8253cbdfbe
devincar <devmcaron@gmail.com>
2025-08-25 19:40:26 -0700

    Added lightmode toggle button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,9 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <ThemeToggle />
+    </div>
+  );
 };
